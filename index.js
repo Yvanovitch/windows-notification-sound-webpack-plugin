2686c728b64b59fda6a9dd34b1b09140ad8df4aa
Yvanovitch <yvan.richer@gmail.com>
2023-04-13 12:59:11 -0400

    Add progression message and platform condition

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const webpack = require('webpack')
 const {exec} = require('child_process')
 
+let previousMessage = '';
+var isWin = process.platform === "win32";
+
 class WindowsNotificationSound {
     constructor(options) {
         return new webpack.ProgressPlugin(function (percent, message, ...args) {
-            if (message === 'done') {
-                exec(`rundll32 user32.dll,MessageBeep`)
+            if (percent == ' 1 ' && isWin) {
+                exec(`powershell "[console]::beep(500,300)"`)
+            }
+            if(message != previousMessage) {
+                console.log('Progression ', percent == ' 1 ' ? '100%' : message,' ', new Date().toLocaleString().replace(',',''))
+                previousMessage = message
             }
         })
     }
